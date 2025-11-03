5075315411656cbc7600f508c90f2cdee8670d7c
Mrowr Purr <mrowr.purr@gmail.com>
2025-11-02 18:15:06 -0800

    text_equals/sender_equals (case insensitive)

--- include/SkyrimScripting/Messages/Message.h
+++ include/SkyrimScripting/Messages/Message.h
@@ -28,11 +28,23 @@ namespace SkyrimScripting::Messages {
         virtual void set_is_request(bool value = true)  = 0;
         virtual void set_reply_id(CallbackID id)        = 0;
 
-        inline bool is_sender(std::string_view name, bool match_case = false) const {
+        inline bool sender_equals(std::string_view name, bool match_case = false) const {
             std::string_view sender_view(sender());
             if (match_case) return sender_view == name;
             return std::equal(
                 sender_view.begin(), sender_view.end(), name.begin(), name.end(),
+                [](char a, char b) {
+                    return std::tolower(static_cast<unsigned char>(a)) ==
+                           std::tolower(static_cast<unsigned char>(b));
+                }
+            );
+        }
+
+        inline bool text_equals(std::string_view msg_text, bool match_case = false) const {
+            std::string_view text_view(text());
+            if (match_case) return text_view == msg_text;
+            return std::equal(
+                text_view.begin(), text_view.end(), msg_text.begin(), msg_text.end(),
                 [](char a, char b) {
                     return std::tolower(static_cast<unsigned char>(a)) ==
                            std::tolower(static_cast<unsigned char>(b));
